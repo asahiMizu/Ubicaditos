1f12f7607aa6eb5abacd2e61f0f8b9024ba4e29e
joseA600 <joseacevedogarcia081@gmail.com>
2025-11-19 00:52:32 -0600

    Update main.py

--- app/main.py
+++ app/main.py
@@ -4,7 +4,10 @@ from app.routers import auth
 app = FastAPI(title="UbicadITOs API")
 
 app.include_router(auth.router, prefix="/auth", tags=["Auth"])
-
+app.include_router(auth.router, prefix="/auth", tags=["Auth"])
+app.include_router(bus.router, prefix="/bus", tags=["Bus"])
+app.include_router(route.router, prefix="/routes", tags=["Routes"])
+app.include_router(driver.router, prefix="/driver", tags=["Driver"])
 @app.get("/")
 def root():
     return {"msg": "API Running"}
